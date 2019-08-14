05e66f70f109bc9be4a7f5b5c56367e853958969
Yazgeldi Seyidov <yazgeldi.seyidov@gmail.com>
2019-08-14 14:33:12 +0000

    Automatically backed up by Learn

--- lib/pry_is_awesome.rb
+++ lib/pry_is_awesome.rb
@@ -2,6 +2,7 @@ require 'pry'
 
 def prying_into_the_method
 	inside_the_method = "We're inside the method"
+	binding.pry
 	puts inside_the_method
 	puts "We're about to stop because of pry!"
 	binding.pry
